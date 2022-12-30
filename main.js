9202d195520274b2ff7cbab78a351a4d6e92a1ac
AmegaPN <95965729+AmegaPN@users.noreply.github.com>
2022-12-30 12:47:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,68 @@ function modelLoaded()
 {
     console.log('Model Loaded!');
 }
+
+function check()
+{
+    img = document.getElementById('captured image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        gesture = results[0].label;
+        toSpeak = "";
+
+        if(gesture == "Amazing")
+        {
+            toSpeak = "This looks amazing";
+            document.getElementById("result_object_gesture_icon").innerHTML = "";
+        }
+
+        if(gesture == "Best")
+        {
+            toSpeak = "All the best";
+            document.getElementById("result_object_gesture_icon").innerHTML = "";
+        }
+
+        if(gesture == "Victory")
+        {
+            toSpeak = "This is a marvelous victory";
+            document.getElementById("result_object_gesture_icon").innerHTML = "";
+        }
+
+        if(gesture == "Thumbs down")
+        {
+            toSpeak = "All the worse";
+            document.getElementById("result_object_gesture_icon").innerHTML = "";
+        }
+
+        if(gesture == "Waving Hand")
+        {
+            toSpeak = "Hello";
+            document.getElementById("result_object_gesture_icon").innerHTML = "";
+        }
+
+        if(gesture == "Raised Fist")
+        {
+            toSpeak = "Want a punch?";
+            document.getElementById("result_object_gesture_icon").innerHTML = "";
+        }
+    }
+}
+
+function speak()
+{
+    var syth = window.speechSynthesis;
+    speak_data = toSpeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    syth.speak(utterThis);
+}
